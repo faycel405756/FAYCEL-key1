e29cc3d7735d2a004aefc20f1efc6b4ad63a6eed
faycel405756 <165543221+faycel405756@users.noreply.github.com>
2026-01-04 22:09:12 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-export async function GET() {
-  const res = await fetch(
-    "https://sieuthidora.io.vn/br1/hma.php?step=1",
-    { redirect: "follow", cache: "no-store" }
-  );
+from flask import Flask, Response
+import requests
+from urllib.parse import urlparse, parse_qs
 
-  const finalUrl = new URL(res.url);
-  const key = finalUrl.searchParams.get("key");
+app = Flask(__name__)
 
-  return new Response(key ?? "NO_KEY", {
-    headers: {
-      "Content-Type": "text/plain",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET",
-    },
-  });
-}
+@app.route("/get-key")
+def get_key():
+    url = "https://sieuthidora.io.vn/br1/hma.php?step=1"
+    r = requests.get(url, allow_redirects=True)
+    params = parse_qs(urlparse(r.url).query)
+
+    key_value = params["key"][0]
+    return Response(f"key: {key_value}", mimetype="text/plain")
+
+if __name__ == "__main__":
+    app.run()
