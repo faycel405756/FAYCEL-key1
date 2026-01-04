6dcfa746a073670e0405ef49edc2a2a5f4e1d24b
faycel405756 <165543221+faycel405756@users.noreply.github.com>
2026-01-04 22:41:29 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask, jsonify
 import requests
 from urllib.parse import urlparse, parse_qs
 
@@ -7,11 +7,18 @@ app = Flask(__name__)
 @app.route("/get-key")
 def get_key():
     url = "https://sieuthidora.io.vn/br1/hma.php?step=1"
-    r = requests.get(url, allow_redirects=True)
+    r = requests.get(url, allow_redirects=True, timeout=10)
+
     params = parse_qs(urlparse(r.url).query)
 
+    if "key" not in params:
+        return jsonify({"error": "key not found"}), 400
+
     key_value = params["key"][0]
-    return Response(f"key: {key_value}", mimetype="text/plain")
+
+    return jsonify({
+        "key": key_value
+    })
 
 if __name__ == "__main__":
     app.run()
